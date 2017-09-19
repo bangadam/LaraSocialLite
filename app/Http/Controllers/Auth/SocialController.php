df97f6feff1f9814b55e339f89d791b32ca15b83
bangadam <bangadam.dev@gmail.com>
2017-09-19 11:59:30 +0700

    finished

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -19,7 +19,7 @@ class SocialController extends Controller
 
     public function handleProviderCallback($provider) {
       $user = Socialite::driver($provider)->user();
-      dd($user);
+      // dd($user);
       $authUser = $this->findOrCreateUser($user, $provider);
       Auth::login($authUser, true);
       return redirect($this->redirectTo);
